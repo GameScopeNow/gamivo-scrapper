d7c6487f1acb5e7bea6480a0be6e041af5f63462
GameScopeNow <167625540+GameScopeNow@users.noreply.github.com>
2024-04-20 15:42:26 +0200

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -1,4 +1,10 @@
 const axios = require('axios');
+const response = await axios.get(url, {
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+    }
+});
+
 
 exports.handler = async function(event, context) {
     const searchTerm = event.queryStringParameters.search;
